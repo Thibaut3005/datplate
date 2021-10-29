08ee1af53695fbdd19ae38a32c0a8cfaa4aa8396
William <william.deville13@gmail.com>
2021-10-29 15:47:45 +0200

    adding svg icons

--- src/client/components/Header.js
+++ src/client/components/Header.js
@@ -1,7 +1,17 @@
 import * as React from 'react'
+import { faHeart, faCartPlus, faUser } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 export default function Header() {
+
+    const Heart = <FontAwesomeIcon icon={faHeart} />;
+    const CartPlus = <FontAwesomeIcon icon={faCartPlus} />;
+    const User = <FontAwesomeIcon icon={faUser} />;
+
     return (
+
+
+
         <header>
             <nav class="header-main-nav">
                 <div class="header-logo">DATPLATE</div>
@@ -9,9 +19,9 @@ export default function Header() {
                     <input class="search-text" type="text" placeholder="Search..."></input>
                 </div>
                 <div class="ul-icons">
-                    <a href="#">Liked</a>
-                    <a href="#">Cart</a>
-                    <a href="#">Sign in</a>
+                    <a href="#"><i className="svg">{Heart}</i>Liked</a>
+                    <a href="#"><i className="svg">{CartPlus}</i>Cart</a>
+                    <a href="#"><i className="svg">{User}</i>Sign in</a>
                 </div>
                 
             </nav>
